771b7693d31580b7bf38942591181caf295d81af
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-03-24 23:11:36 +0000

    Mon, Mar 25, 2024, 1:11 AM +02:00

---
+++ triangle.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+
+{
+    int x=get_int (x= );
+    int y=get_int (y= );
+    int z=get_int (z= );
+
+    if (x>y);
+    {
+    return=0;
+    }
+    else if (y>x);
+    {
+    return=0;
+    }
+    else if (z>x);
+    {
+    return=0;
+    }
+    else
+}
