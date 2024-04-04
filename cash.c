ec4313f0a546ce2b4a81c39145e129261a7fcd2f
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-04 23:40:00 +0000

    Fri, Apr 5, 2024, 1:40 AM +02:00

--- cash.c
+++ cash.c
@@ -10,10 +10,10 @@ int main (void)
         n=get_int("change owned: ");
     }
 
-    float x=(n/25);
-    float y=(n/10 - x*2.5);
-    float z=(n/5 - x*5);
-    float v=(n/1 - x*25);
+    int x=(n/25);
+    int y=(n/10 - x*2.5);
+    int z=(n/5 - x*5);
+    int v=(n/1 - x*25);
     int i=(x+y+z+v);
     printf("%i\n",i);
 
