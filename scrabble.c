0a71041c3e3f4f3dfb591c5c268e9c5fb487e12a
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-05 00:56:31 +0000

    Fri, Apr 5, 2024, 2:56 AM +02:00

--- scrabble.c
+++ scrabble.c
@@ -6,11 +6,11 @@ int main(void)
 
 {
     string s=get_string("input: ");
-    for (int i=0; n = strlen(s); i<n; i++)
+    for (int i=0, n = strlen(s); i<n; i++)
     {
         if (s[i]>='a' && s[i]<='z')
         {
-            printf("%c",s[i]-32)
+            printf("%c",s[i]-32);
         }
         else
         {
