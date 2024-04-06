7a9d8dc1a9b119fc6e9c4c295ba59d0e4e93f536
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-06 18:25:10 +0000

    Sat, Apr 6, 2024, 8:25 PM +02:00

--- scrabble.c
+++ scrabble.c
@@ -7,14 +7,7 @@ int points=[1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
 int main(void)
 
 {
-    string s = get_string ("player 1:");
-    string z = get_string ("player 2:");
-
-    if (int i >= 'a' && i <= 'z')
-    {
-    for (int i=0, n=strlen (s); i<n; i++)
-    {
-        s[i] -= 32
-    }
-    }
+    string ask1= get_string ("player 1: ");
+    string ask2= get_string ("player 2: ");
+    
 }
