69373a429e8560efbbb557f0e78d197f883462b7
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-06 18:55:19 +0000

    Sat, Apr 6, 2024, 8:55 PM +02:00

--- scrabble.c
+++ scrabble.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int points=[1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10];
-int compute_score(compute);
+int points[] ={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+int compute_score(word);
 
 int main(void)
 
