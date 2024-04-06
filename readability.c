a10b2b8e4361b74932432e51b7b80d4e47f829fe
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-06 19:31:10 +0000

    Sat, Apr 6, 2024, 9:31 PM +02:00

--- readability.c
+++ readability.c
@@ -3,13 +3,23 @@
 #include <string.h>
 #include <stdio.h>
 
-int main (void)
+int compute_alpha(string letter);
 
+int main (void)
+{
+
+}
+
+int compute_alpha(string letter)
 {
     string s = get_string ("text: ");
+    int alpha=0;
 
     for (int i=0, len=strlen(s); i<len; i++)
     {
-        
+        if (isalpha(s[i]))
+        {
+            alpha += 1;
+        }
     }
 }
