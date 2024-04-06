a7b0cecbcdfc31426812213a46f54323ad7ee4dc
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-06 20:36:20 +0000

    Sat, Apr 6, 2024, 10:36 PM +02:00

--- readability.c
+++ readability.c
@@ -18,7 +18,7 @@ int main (void)
     float L = letters/words * 100;
     float S = sentences/words *100;
     float index = 0.0588 * L - 0.296 * S - 15.8;
-    float Z = float roundf(float index);
+    int Z = index + 0.5;
 
     if (index<1)
     {
