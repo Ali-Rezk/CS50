e6a796bf1a7801fbb78be7048f20b54b9e67466a
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-06 18:56:34 +0000

    Sat, Apr 6, 2024, 8:56 PM +02:00

--- scrabble.c
+++ scrabble.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int points[] ={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-int compute_score(word);
+int compute_score(string word);
 
 int main(void)
 
@@ -30,7 +30,7 @@ int main(void)
 
     printf("\n");
 }
-int compute_score(word)
+int compute_score(string word)
 {
     int score = 0;
 
