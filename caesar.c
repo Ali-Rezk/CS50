62a1d8434331333da07266987cf05d2899655021
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-08 23:56:12 +0000

    Tue, Apr 9, 2024, 1:56 AM +02:00

--- caesar.c
+++ caesar.c
@@ -4,19 +4,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 int main(int argc,string argv[])
 {
     string s = get_string("plaintext:  ");
-    atoi(argv[]);
-    
+
     if (argc>2 || argc<2)
     {
         printf{"Usage: ./caesar key"};
     }
-    else if (argv[1]<0)
+    else if (isalpha(argv[1]))
     {
         printf{"Usage: ./caesar key"};
     }
-
+    else if (atoi(argv[1]);<0)
+    {
+        printf{"Usage: ./caesar key"};
+    }
 }
 
