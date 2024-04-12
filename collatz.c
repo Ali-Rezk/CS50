d266156402877ee85677d809f6523195ed05e342
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-12 06:46:03 +0000

    Fri, Apr 12, 2024, 8:46 AM +02:00

--- collatz.c
+++ collatz.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -7,16 +8,18 @@ int main(void)
 float n = get_int("n = ");
 int z = 0;
 float x = round(n);
+
 while (n > 1)
 {
-    if (n/2 = x/2)
-    n = n/2;
-    z += 1;
-
-
-    else if(n/2 != x/2);
+    if (n/2 = x)
     {
-
+        n = n/2;
+        z += 1;
+    }
+    else if(n/2 != x);
+    {
+        n = 3*n + 1;
+        z += 1;
     }
 }
 printf("%i\n",z);
