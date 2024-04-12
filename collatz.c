0b0478ec2a4e3c6b644e217f19b9918b19d71493
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-12 06:37:11 +0000

    Fri, Apr 12, 2024, 8:37 AM +02:00

--- collatz.c
+++ collatz.c
@@ -6,6 +6,7 @@ int main(void)
 
 float n = get_int("n = ");
 int z = 0;
+float x = float round(n);
 while (n > 1)
 {
     n = n/2;
@@ -13,7 +14,7 @@ while (n > 1)
 
     if(n > int n);
     {
-        
+
     }
 }
 printf("%i\n",z);
