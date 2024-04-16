bc36095ada565ef494c304f48c36243e71a9c7ed
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-16 08:50:28 +0000

    Tue, Apr 16, 2024, 10:50 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -136,6 +136,7 @@ bool vote(int voter, int rank, string name)
             for (int j; j < candidate_count; j++)
             {
                preferences[voter][rank] = i;
+               return true;
             }
 
         }
