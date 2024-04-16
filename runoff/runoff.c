4558f25e0a1dc134f6a4ff798ca3e4b124595b62
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-16 15:08:18 +0000

    Tue, Apr 16, 2024, 5:08 PM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -226,7 +226,7 @@ int find_min(void)
     string loser;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].eliminated == false && candidates[i].votes < min)
+        if (!candidates[i].eliminated && candidates[i].votes < min)
         {
             min = candidates[i].votes;
         }
