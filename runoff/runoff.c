74e736e589ee0fbf7937c59045401630ec4cbadc
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-16 09:10:54 +0000

    Tue, Apr 16, 2024, 11:10 AM +02:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -148,13 +148,16 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < voter_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (preferences[i][j] == i)
+            for (int k; k < candidate_count; k++)
             {
-                
+                if (preferences[i][j] == k)
+                {
+                    candidates[k].vote += 1;
+                }
             }
         }
     }
