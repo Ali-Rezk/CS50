71647f24502e60188bb36fabf4e71c4dffd6c529
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-21 20:36:03 +0000

    Sun, Apr 21, 2024, 10:36 PM +02:00

--- test.c
+++ test.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap (int *a, int *b);
+
 int main(void)
 {
-    printf("This is CS%i\n", atoi("50awS"));
+    int a = 10;
+    int b = 50;
+    printf("a is %i, b is %i\n", a , b);
+}
+
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
