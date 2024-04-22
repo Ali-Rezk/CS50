e97e40d6b7d44586a2da0b4b2b1b6c4c12f53046
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-22 17:39:20 +0000

    Mon, Apr 22, 2024, 7:39 PM +02:00

--- mario.c
+++ mario.c
@@ -8,26 +8,26 @@ void print_bricks(int bricks);
 int main(void)
 
 {
-    int n=get_int("height:");
-    for (int i=0; i<n; i++)
+    int n = get_int("height:");
+    for (int i = 0; i < n; i++)
     {
-        print_bricks(n-i-1);
-        print_rows(i+1);
+        print_bricks(n - i - 1);
+        print_rows(i + 1);
         printf(" ");
-        print_rows(i+1);
+        print_rows(i + 1);
         printf("\n");
     }
 }
 void print_rows(int length)
 {
-    for (int i=0; i<length; i++)
+    for (int i = 0; i < length; i++)
     {
         printf("#");
     }
 }
 void print_bricks(int bricks)
 {
-    for (int i=0; i<bricks; i++)
+    for (int i = 0; i < bricks; i++)
     {
         printf(" ");
     }
