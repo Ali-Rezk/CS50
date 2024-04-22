e131aadf99572910be081e3e7e9ed8d99eb2d03e
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-22 17:19:46 +0000

    Mon, Apr 22, 2024, 7:19 PM +02:00

--- scrabble.c
+++ scrabble.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int points[] ={1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+int points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int compute_score(string word);
 
 int main(void)
 
 {
-    string ask1= get_string ("player 1: ");
-    string ask2= get_string ("player 2: ");
+    string ask1 = get_string("player 1: ");
+    string ask2 = get_string("player 2: ");
 
-    int score1 = compute_score (ask1);
-    int score2 = compute_score (ask2);
+    int score1 = compute_score(ask1);
+    int score2 = compute_score(ask2);
 
-    if (score1>score2)
+    if (score1 > score2)
     {
         printf("player 1 wins");
     }
-    else if (score2>score1)
+    else if (score2 > score1)
     {
         printf("Player 2 wins");
     }
@@ -34,17 +34,16 @@ int compute_score(string word)
 {
     int score = 0;
 
-    for (int i=0, n=strlen (word); i < n; i++)
+    for (int i = 0, n = strlen(word); i < n; i++)
     {
         if (isupper(word[i]))
         {
-            score += points[word[i]-'A'];
+            score += points[word[i] - 'A'];
         }
         else if (islower(word[i]))
         {
-            score += points[word[i]-'a'];
+            score += points[word[i] - 'a'];
         }
-
     }
     return score;
 }
