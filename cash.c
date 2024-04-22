ea50804e2037019d5592b44bc247a8f530c2cc13
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-22 17:47:41 +0000

    Mon, Apr 22, 2024, 7:47 PM +02:00

--- cash.c
+++ cash.c
@@ -1,20 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (void)
+int main(void)
 
 {
-    int n=get_int("change owned: ");
-    while (n<0)
+    int n = get_int("change owned: ");
+    while (n < 0)
     {
-        n=get_int("change owned: ");
+        n = get_int("change owned: ");
     }
 
-    int x=(n/25);
-    int y=(n/10 - x*2.5);
-    int z=(n/5 - y*2 - x*5);
-    int v=(n/1 - z*5 - y*10 - x*25);
-    int i=(x+y+z+v);
-    printf("%i\n",i);
-
+    int x = (n / 25);
+    int y = (n / 10 - x * 2.5);
+    int z = (n / 5 - y * 2 - x * 5);
+    int v = (n / 1 - z * 5 - y * 10 - x * 25);
+    int i = (x + y + z + v);
+    printf("%i\n", i);
 }
