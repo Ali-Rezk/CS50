504ac58212b3c5aac067448909319daf519ddaac
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-22 16:53:32 +0000

    Mon, Apr 22, 2024, 6:53 PM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,11 +43,11 @@ int main(int argc, char *argv[])
     fwrite(&buffer,HEADER_SIZE,1,output);
 
     // TODO: Read samples from input file and write updated data to output file
-    int* b = malloc(sizeof(dbyte));
-
+    int b []= malloc(sizeof(dbyte));
+    int* a = b;
     while (fread(b,2,1,input))
     {
-        int a = b * factor;
+        a = b * factor;
         fwrite(&a,2,1,output);
     }
 
