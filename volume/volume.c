ddfea7d0435f9dbacf4f34a43c34833956140edb
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-22 16:07:02 +0000

    Mon, Apr 22, 2024, 6:07 PM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -43,6 +43,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     
+    for (int i = 0;)
     // Close files
     fclose(input);
     fclose(output);
