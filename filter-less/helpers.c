dd3eda1a13db55a3e5045793794e537249509e03
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-24 13:32:49 +0000

    Wed, Apr 24, 2024, 3:32 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -90,7 +90,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            r = image[i][j].rgbtRed + image[i + 1][j].rgbtRed + image[i - 1][j].rgbtRed + image[i][j].rgbtRed +
+            r = image[i][j].rgbtRed + image[i + 1][j].rgbtRed + image[i - 1][j].rgbtRed + image[i][j + 1].rgbtRed + image[i][j - 1].rgbtRed +image[i + 1][j + 1].rgbtRed + image[i + 1][j - 1].rgbtRed + image[i][j].rgbtRed
             g = image[i][j].rgbtGreen;
             b = image[i][j].rgbtBlue;
         }
