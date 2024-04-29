43a261d1a356a5b5083d4b06eecb57cebd74632b
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-29 13:13:08 +0000

    Mon, Apr 29, 2024, 4:13 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,16 +7,17 @@ int main(int argc, char *argv[])
 {
     char* infile = argv[1];
     FILE* src = fopen(infile, "r");
+    FILE* dst;
     byte buffer[512];
 
     while (fread(buffer,512,1,src) != 0)
     {
         if (buffer[0] == 0xff & buffer[1] == 0xd8 & buffer[2] == 0xff & (buffer[3] & 0xf0) == 0xe0 )
         {
-            sprintf(filename, "%03i.jpg", 2);
+            sprintf(dst, "%03i.jpg", 2);
         }
 
-        FILE* img = fopen(filename, "w");
+        FILE* img = fopen(dst, "w");
         fwrite(buffer,512,1,img);
     }
 }
