ecab40eb11da2ce8ade9217f1fadc631671cf711
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-04-29 12:03:34 +0000

    Mon, Apr 29, 2024, 3:03 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef uint8_t byte;
+
 int main(int argc, char *argv[])
 {
     char* infile = argv[1];
-
     FILE* src = fopen(infile,"r");
+    byte buffer[512];
 
-    while (fread())
+    while (fread(buffer,512,1,src) != 0)
+    {
+        
+    }
 }
