87e4a3a706fa5b9ee09899bd77899c2baa9554b7
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-05 16:44:30 +0000

    Wed, Jun 5, 2024, 7:44 PM +03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -1,11 +1,8 @@
 // Implements a dictionary's functionality
-#include <cs50.h>
+
 #include <ctype.h>
-#include <stdio.h>
-#include <string.h>
-#include <strings.h>
-#include <stdlib.h>
 #include <stdbool.h>
+
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -16,114 +13,42 @@ typedef struct node
 } node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 52;
+const unsigned int N = 26;
 
 // Hash table
 node *table[N];
-node *n;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
-    node *cursor = table[hash(word)];
-
-    while (cursor != NULL)
-    {
-        int z = strcasecmp(cursor->word, word);
-        cursor = cursor->next;
-
-        if (z == 0)
-        {
-            return true;
-        }
-    }
-
-     return false;
+    return false;
 }
-
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    int z = 0;
-
-    for (int i = 0, len = strlen(word); i < len; i++)
-    {
-        int x = toupper(word[i]) - 'A';
-        z += x;
-    }
-
-    return z % N;
+    return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO create hash table
-    char word[LENGTH + 1];
-    FILE *file = fopen(dictionary,"r");
-    int z = 0;
-
-    if (file == NULL)
-    {
-        return false;
-    }
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        n = malloc(sizeof(node));
-        if (n == NULL)
-        {
-            return false;
-        }
-        z = hash(word);
-        strcpy(n->word, word);
-        n->next = table[z];
-        table[z] = n;
-    }
-    fclose(file);
-    return true;
+    // TODO
+    return false;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
     // TODO
-    FILE * file = fopen("dictionaries/large","r");
-    char word[LENGTH + 1];
-    int x = 0;
-
-    if (file == NULL)
-    {
-        return 0;
-    }
-
-    while (fscanf(file, "%s", word) != EOF)
-    {
-        x++;
-
-    }
-
-    fclose(file);
-    return x;
+    return 0;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-
-    for (int i = 0; i < N; i++)
-    {
-        n = table[i];
-
-        while (n != NULL)
-        {
-            table[i] = n->next;
-            free(n);
-            n = table[i];
-        }
-    }
-    return true;
+    return false;
 }
