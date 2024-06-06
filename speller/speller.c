247a92d9eef29b840d6cd55b9c835a59e31a7f33
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-06 22:39:06 +0000

    Fri, Jun 7, 2024, 1:39 AM +03:00

--- speller/speller.c
+++ speller/speller.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
     double time_load = 0.0, time_check = 0.0, time_size = 0.0, time_unload = 0.0;
 
     // Determine dictionary to use
-    printf("shshshsh %s", argv[2]);
+    printf("shshshsh %s", argv[1]);
     printf("shshshsh %d", argc);
     char *dictionary = (argc == 3) ? argv[1] : DICTIONARY;
 
