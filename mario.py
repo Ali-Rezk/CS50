f55091e8143cefb1d1169935b7fb0ddf54255c6f
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-10 11:57:35 +0000

    Mon, Jun 10, 2024, 2:57 PM +03:00

--- mario.py
+++ mario.py
@@ -2,10 +2,14 @@ def main():
     height = int(input("Height: "))
     for i in range(height):
         brick(i)
+        print(" ", end="")
     print()
-    
+
 def brick(n):
     for i in range(n):
         print("#", end="")
+    print()
+
+
 
 main()
