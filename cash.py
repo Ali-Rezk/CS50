52f837ef39850b34d33a812093b144876234ff2a
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-10 14:07:18 +0000

    Mon, Jun 10, 2024, 5:07 PM +03:00

--- cash.py
+++ cash.py
@@ -9,7 +9,7 @@ while n < 0:
 x = int((n / 0.25))
 y = int((n / 10 - x * 2.5))
 z = int((n / 5 - y * 2 - x * 5))
-v = int((n / 1 - z * 5 - y * 10 - x * 2.5))
+v = int((n / 1 - z * 5 - y * 10 - x * 25))
 i = int((x + y + z + v))
 
 print(f"{i}")
