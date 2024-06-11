2b54c3c37e2a757e3fd9226a546c83c581f4c1d0
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-11 15:35:50 +0000

    Tue, Jun 11, 2024, 6:35 PM +03:00

--- readability.py
+++ readability.py
@@ -2,4 +2,26 @@
 
 
 def main():
-    
+    s = input("text: ");
+    words = compute_spaces(s);
+    letters = compute_alpha(s);
+    sentences = compute_punct(s);
+    L = letters / words * 100;
+    S = sentences / words * 100;
+    index = 0.0588 * L - 0.296 * S - 15.8;
+    Z = (float) round(index);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+    else if (Z > 16)
+    {
+        printf("Grade 16+");
+    }
+    else
+    {
+        printf("Grade %i", Z);
+    }
+
+    printf("\n");
