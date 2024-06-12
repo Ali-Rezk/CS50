d0026afe9e13a78c793320ab7caa67a6b5a27a9d
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-12 00:52:04 +0000

    Wed, Jun 12, 2024, 3:52 AM +03:00

--- cash.py
+++ cash.py
@@ -5,7 +5,7 @@ n = get_float("change owned: ")
 
 while n < 0:
     n = get_float("change owned: ")
-n = 0.15/0.03
+n = 0.15//0.05
 x = int((n / 0.25))
 y = int((n / 0.10 - x * 2.5))
 z = int((n // 0.05 + 0.0001 - y * 2.0 - x * 5.0))
