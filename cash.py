1dfb0f7d93fcff067894307bf3e3b8f3cc38790d
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-12 00:55:44 +0000

    Wed, Jun 12, 2024, 3:55 AM +03:00

--- cash.py
+++ cash.py
@@ -1,5 +1,5 @@
 from cs50 import get_float
-
+from decimal import Decimal
 
 n = get_float("change owned: ")
 
@@ -8,7 +8,7 @@ while n < 0:
 n = 0.15//0.05
 x = int((n / 0.25))
 y = int((n / 0.10 - x * 2.5))
-z = int((n // 0.05 + 0.0001 - y * 2.0 - x * 5.0))
+z = int((Decimal(n) / Decimal('0.05') + 0.0001 - y * 2.0 - x * 5.0))
 v = int((n / 0.01 - z * 5.0 - y * 10.0 - x * 25.0))
 i = int((x + y + z + v))
 
