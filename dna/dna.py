2f0a2969e989fcf67a9b96210e066126549b97c7
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-14 20:41:13 +0000

    Fri, Jun 14, 2024, 11:41 PM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -17,17 +17,12 @@ def main():
         seq = sequence.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    x = 0
-    maxvalue = 0
-    for i in seq:
-        if seq[i] == 'A' and seq[i+1] == 'G' and seq[i+2] == "A":
-            if seq[i+3] == "T" and seq[i+4] == "C":
-                x += 1
-                maxvalue = x
+    agatc = longest_match(seq, "AGATC")
+    aatg = longest_match(seq, "AATG")
+    tatc = longest_match(seq, "TATC")
 
-        x = 0
     # TODO: Check database for matching profiles
-
+    if agatc == 
     return
 
 
