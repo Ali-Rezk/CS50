72b34d090e8e4ebeaf5cdc27302fdf5afb62de20
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-14 21:18:12 +0000

    Sat, Jun 15, 2024, 12:18 AM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(argv) > 3 or len(argv) < 3:
+    if len(sys.argv) > 3 or len(sys.argv) < 3:
         print("commandline error")
 
     # TODO: Read database file into a variable
