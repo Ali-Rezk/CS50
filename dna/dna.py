0be37a953924818370f2741826814d5862234aed
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-06-14 21:12:41 +0000

    Sat, Jun 15, 2024, 12:12 AM +03:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,11 +12,11 @@ def main():
     sequence = sys.argv[2]
 
     # TODO: Read database file into a variable
-    with open(sys.argv[1]) as file:
+    with open("large.csv") as file:
         data = csv.DictReader(file)
 
     # TODO: Read DNA sequence file into a variable
-    with open(sys.argv[2]) as file:
+    with open("sequences/5.txt") as file:
         seq = sequence.read()
 
     # TODO: Find longest match of each STR in DNA sequence
