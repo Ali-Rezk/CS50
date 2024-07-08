8d25c65640bf40508143ff29b7463d207f7b1ed7
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 23:26:59 +0000

    Tue, Jul 9, 2024, 2:26 AM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,19 +1,12 @@
 SELECT name
-FROM stars
-WHERE name = (
-    SELECT name FROM people WHERE id = (
-        SELECT person_id FROM stars WHERE movie_id = (
-            SELECT movie_id FROM stars WHERE person_id = (
-                SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958))))
-AND name NOT in ('Kevin Bacon') GROUP BY name;
-
-
-SELECT name
 FROM people
 WHERE id = (
     SELECT person_id FROM stars WHERE movie_id = (
         SELECT movie_id FROM stars WHERE person_id = (
             SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958)))
 AND name NOT in ('Kevin Bacon') GROUP BY name;
+
+
+
 
 
