df8811e57d4a162da902602c8e0274e62c5c95d0
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 21:50:39 +0000

    Tue, Jul 9, 2024, 12:50 AM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,6 +1,7 @@
 SELECT name
 FROM stars
-JOIN people ON person_id = id
+JOIN people p ON person_id = p.id
+JOIN movies m ON movie_id = m.id
 WHERE person_id = (
     SELECT id FROM people WHERE name = 'Kevin Bacon'
 )
