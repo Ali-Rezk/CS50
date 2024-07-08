82c51665aeb5f168eb8aa4f5e71e7feb5ea872e0
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 22:42:38 +0000

    Tue, Jul 9, 2024, 1:42 AM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,9 +1,10 @@
 SELECT name
 FROM stars
 JOIN people ON person_id = id
-WHERE movie_id = (
-    SELECT id FROM stars WHERE person_id = (
-        SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958))
+WHERE id = (
+    SELECT person_id FROM stars WHERE movie_id = (
+        SELECT id FROM stars WHERE person_id = (
+            SELECT id FROM people WHERE name = 'Kevin Bacon' AND birth = 1958)))
 AND name NOT in ('Kevin Bacon');
 
 
