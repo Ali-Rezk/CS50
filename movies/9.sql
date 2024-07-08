1f83a789f3e9da1c054b6c756afd80b84c379c6b
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 18:44:43 +0000

    Mon, Jul 8, 2024, 9:44 PM +03:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,2 +1,2 @@
-SELECT name FROM stars JOIN people ON person_id = id WHERE movie_id = (
-    SELECT id FROM movies WHERE year = 2004) ORDER BY birth ;
+SELECT name FROM ((stars JOIN people ON person_id = people.id) JOIN movies ON movie_id = movies.id)
+WHERE year = 2004 ORDER BY birth;
