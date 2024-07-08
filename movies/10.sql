ad2e91064a7e404e773f0cf1aff9ec62f20ae6f5
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 18:48:25 +0000

    Mon, Jul 8, 2024, 9:48 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,2 +1,1 @@
-SELECT name FROM directors JOIN people ON id = person_id where movie_id = (
-    SELECT movie_id FROM ratings WHERE rating >= 9.0);
+SELECT name FROM ((directors JOIN people ON person_id = id) JOIN ratings ON directors.movie_id = ratings.movies_id) WHERE rating >= 9.0;
