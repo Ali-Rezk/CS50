616a0f748b5a916b0598669b2fc8a8727ff0bb6a
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 21:46:10 +0000

    Tue, Jul 9, 2024, 12:46 AM +03:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,7 +1,7 @@
 SELECT name
-FROM people
-WHERE id = (
-    
+FROM stars
+JOIN people ON person_id = id
+WHERE person_id = (
+    SELECT id FROM people WHERE name = 'Kevin Bacon'
 )
-
 WHERE name NOT = Kevin Bacon
