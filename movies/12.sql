7ef63bd7fe43dcaa624db520c8ff940d08dc6e23
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 19:40:14 +0000

    Mon, Jul 8, 2024, 10:40 PM +03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,2 @@
 SELECT title FROM ((stars JOIN movies ON movie_id = movies.id) JOIN people ON person_id = people.id)
-    WHERE name = 'Bradley Cooper' AND 'Jennifer Lawrence';
+    WHERE name = 'Bradley Cooper' OR 'Jennifer Lawrence';
