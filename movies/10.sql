25f83c82e77b9487dc70cd98ad9e04b0c829892a
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-08 18:50:13 +0000

    Mon, Jul 8, 2024, 9:50 PM +03:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,1 @@
-SELECT name FROM ((directors JOIN people ON person_id = id) JOIN ratings ON directors.movie_id = ratings.movies_id) WHERE rating >= 9.0;
+SELECT name FROM ((directors JOIN people ON person_id = id) JOIN ratings ON directors.movie_id = ratings.movie_id) WHERE rating >= 9.0;
