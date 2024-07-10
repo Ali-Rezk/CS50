7a0047ca0469a27779f90ac3f623f58004357f53
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 20:19:18 +0000

    Wed, Jul 10, 2024, 11:19 PM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,5 +1,9 @@
 SELECT *
 FROM people
+SELECT *
+FROM bank_accounts
+JOIN people ON person_id = id
+WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199);
 WHERE phone_number = (
     SELECT caller
     FROM phone_calls
