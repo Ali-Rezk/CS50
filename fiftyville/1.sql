1ba1098c67ad517b72ae6a40b2f3b5b6ea80dbc1
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 21:17:32 +0000

    Thu, Jul 11, 2024, 12:17 AM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -2,7 +2,7 @@ SELECT DISTINCT name
 FROM people p1
 JOIN bakery_security_logs b ON p1.license_plate = b.license_plate
 JOIN phone_calls p2 ON p1.phone_number = p2.caller
-JOIN passengers ON 
+JOIN passengers PS ON P1.passport_number = PS.passport_number
 WHERE duration < 60
 AND b.day = 28 AND b.month = 7
 AND b.year = 2023 AND hour = 10
