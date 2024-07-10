aadb942a9e8c074c2a43cad9ccf833b7c08aa9f6
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 20:27:41 +0000

    Wed, Jul 10, 2024, 11:27 PM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,23 +1,23 @@
 SELECT *
 FROM people
 JOIN bank_accounts ON id = person_id
-WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199);
+WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
 AND phone_number = (
     SELECT caller
     FROM phone_calls
     WHERE day = 28 AND month = 7
-    AND year = 2023 AND duration < 60;
-)
-OR (
-    SELECT receiver
-    FROM phone_calls
-    WHERE day = 28 AND month = 7
-    AND year = 2023 AND duration < 60;
+    AND year = 2023 AND duration < 60
 )
 AND license_plate = (
     SELECT license_plate
     FROM bakery_security_logs
     WHERE day = 28 AND month = 7
     AND year = 2023 AND hour = 10
-    AND minute >= 15 AND minute <= 25;
+    AND minute >= 15 AND minute <= 25
+    )
+AND passport_number = (
+    SELECT passport_number
+    FROM passengers
+    WHERE flight_id = 36
+)
 ;
