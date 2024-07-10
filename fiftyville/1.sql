08d934fd88be9f55094ff5a4315c7cc1f435fa81
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 20:53:50 +0000

    Wed, Jul 10, 2024, 11:53 PM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -2,4 +2,11 @@ SELECT *
 FROM people
 JOIN bank_accounts ON id = person_id
 WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
+AND license_plate = (
+    SELECT license_plate
+    FROM bakery_security_logs
+    WHERE day = 28 AND month = 7
+    AND year = 2023 AND hour = 10
+    AND minute >= 15 AND minute <= 25
+)
 ;
