dbd222696e4eb787deaa4a2e7f563bfd4b5a2a16
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 21:19:20 +0000

    Thu, Jul 11, 2024, 12:19 AM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -3,9 +3,11 @@ FROM people p1
 JOIN bakery_security_logs b ON p1.license_plate = b.license_plate
 JOIN phone_calls p2 ON p1.phone_number = p2.caller
 JOIN passengers PS ON P1.passport_number = PS.passport_number
+JOIN bank_accounts ON p1.id = person_id
 WHERE duration < 60
 AND b.day = 28 AND b.month = 7
 AND b.year = 2023 AND hour = 10
 AND minute >= 15 AND minute <= 25
 AND flight_id = 36
+AND account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
 ;
