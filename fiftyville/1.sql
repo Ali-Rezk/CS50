52d4472d15871f1ba53af9083c035867864bad1f
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 21:33:39 +0000

    Thu, Jul 11, 2024, 12:33 AM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,8 +1,12 @@
-SELECT DISTINCT *
-FROM people
-WHERE phone_number = (
-    SELECT receiver
-    FROM phone_calls
-    WHERE receiver = '(375) 555-8161'
-)
-;
+SELECT DISTINCT name, p1.license_plate, p1.phone_number, account_number, p1.passport_number
+FROM people p1
+JOIN bakery_security_logs b ON p1.license_plate = b.license_plate
+JOIN phone_calls p2 ON p1.phone_number = p2.caller
+JOIN passengers PS ON P1.passport_number = PS.passport_number
+JOIN bank_accounts b2 ON p1.id = b2.person_id
+WHERE duration < 60
+AND b.day = 28 AND b.month = 7
+AND b.year = 2023 AND hour = 10
+AND minute >= 15 AND minute <= 25
+AND flight_id = 36
+AND account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199);
--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -39,11 +39,11 @@ AND year = 2023;
 SELECT *
 FROM airports
 WHERE id IN (8, 4);
---getting passport numbers
+--getting passport numbers.
 SELECT *
 FROM passengers
 WHERE flight_id = 36;
---identifying the thief
+--identifying the thief.
 SELECT DISTINCT name, p1.license_plate, p1.phone_number, account_number, p1.passport_number
 FROM people p1
 JOIN bakery_security_logs b ON p1.license_plate = b.license_plate
@@ -56,3 +56,11 @@ AND b.year = 2023 AND hour = 10
 AND minute >= 15 AND minute <= 25
 AND flight_id = 36
 AND account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199);
+--identifying the accomplice.
+SELECT name
+FROM people
+WHERE phone_number = (
+    SELECT receiver
+    FROM phone_calls
+    WHERE receiver = '(375) 555-8161'
+);
