bd55e9ef6fadc7fe3d1976f733f486d2ea59c97c
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 21:16:34 +0000

    Thu, Jul 11, 2024, 12:16 AM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,12 +1,11 @@
-SELECT name
+SELECT DISTINCT name
 FROM people p1
-JOIN bakery_security_logs b ON p.license_plate = b.license_plate
-JOIN atm_transactions a ON p.day = a.day
+JOIN bakery_security_logs b ON p1.license_plate = b.license_plate
 JOIN phone_calls p2 ON p1.phone_number = p2.caller
+JOIN passengers ON 
 WHERE duration < 60
-AND P1.day = 28 AND month = 7
-AND year = 2023 AND hour = 10
+AND b.day = 28 AND b.month = 7
+AND b.year = 2023 AND hour = 10
 AND minute >= 15 AND minute <= 25
-AND atm_location = 'Leggett Street'
-AND transaction_type = 'withdraw'
+AND flight_id = 36
 ;
