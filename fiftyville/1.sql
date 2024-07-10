07228e21b35960ad50b5b10c4afdb07a59e2ec75
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 20:45:33 +0000

    Wed, Jul 10, 2024, 11:45 PM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,22 +1,5 @@
 SELECT *
 FROM people
-WHERE id = (
-    SELECT person_id
-    FROM bank_accounts
-    WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
-)
-AND phone_number = (
-    SELECT caller
-    FROM phone_calls
-    WHERE day = 28 AND month = 7
-    AND year = 2023 AND duration < 60
-)
-AND license_plate = (
-    SELECT license_plate
-    FROM bakery_security_logs
-    WHERE day = 28 AND month = 7
-    AND year = 2023 AND hour = 10
-    AND minute >= 15 AND minute <= 25
-    )
-
+JOIN bank_accounts ON id = person_id
+WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
 ;
