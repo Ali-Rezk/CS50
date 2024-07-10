158f7929c7d91a6be063155ae7d6bf67187132e6
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 21:11:44 +0000

    Thu, Jul 11, 2024, 12:11 AM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,11 +1,12 @@
-SELECT *
-FROM people p
+SELECT name
+FROM people p1
 JOIN bakery_security_logs b ON p.license_plate = b.license_plate
-JOIN atm_transactions ON
-WHERE day = 28 AND month = 7
-AND year = 2023 AND atm_location = 'Leggett Street'
-AND transaction_type = 'withdraw';
-WHERE day = 28 AND month = 7
+JOIN atm_transactions a ON p.day = a.day
+JOIN phone_calls p2 ON p1.phone_number = p2.caller
+WHERE duration < 60
+AND P1.day = 28 AND month = 7
 AND year = 2023 AND hour = 10
 AND minute >= 15 AND minute <= 25
+AND atm_location = 'Leggett Street'
+AND transaction_type = 'withdraw'
 ;
