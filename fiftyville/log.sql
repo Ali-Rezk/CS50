6b10d49f366ca3115fbc49b63524684689fc6154
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 20:59:48 +0000

    Wed, Jul 10, 2024, 11:59 PM +03:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,8 +3,8 @@ SELECT description
 FROM crime_scene_reports
 WHERE day = 28 AND month = 7
 AND year = 2023 AND street = 'Humphrey Street';
--- getting the name and transcript of the 3 witnesses.
-SELECT name, transcript
+-- getting the transcript of the 3 witnesses.
+SELECT transcript
 FROM interviews
 WHERE day = 28 AND month = 7
 AND year = 2023;
