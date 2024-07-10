9e8c3141f953af72ed97df5896d34df332c26e02
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 20:43:55 +0000

    Wed, Jul 10, 2024, 11:43 PM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -1,7 +1,10 @@
 SELECT *
 FROM people
-JOIN bank_accounts ON id = person_id
-WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
+WHERE id = (
+    SELECT person_id
+    FROM bank_accounts
+    WHERE account_number IN (28500762, 28296815, 76054385, 49610011, 16153065, 25506511, 81061156, 26013199)
+)
 AND phone_number = (
     SELECT caller
     FROM phone_calls
@@ -15,9 +18,5 @@ AND license_plate = (
     AND year = 2023 AND hour = 10
     AND minute >= 15 AND minute <= 25
     )
-AND passport_number = (
-    SELECT passport_number
-    FROM passengers
-    WHERE flight_id = 36
-)
+
 ;
