32099fae6b6e6da0d22d632c32ae590fc7c0db05
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-10 18:02:04 +0000

    Wed, Jul 10, 2024, 9:02 PM +03:00

--- fiftyville/1.sql
+++ fiftyville/1.sql
@@ -2,4 +2,4 @@ SELECT *
 FROM bakery_security_logs
 WHERE day = 28 AND month = 7
 AND year = 2023 AND hour = 10
-;
+AND minute >= 15 AND minute <= 25;
--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -8,3 +8,9 @@ SELECT name, transcript
 FROM interviews
 WHERE day = 28 AND month = 7
 AND year = 2023;
+--getting info about the car of the thief.
+SELECT *
+FROM bakery_security_logs
+WHERE day = 28 AND month = 7
+AND year = 2023 AND hour = 10
+AND minute >= 15 AND minute <= 25;
