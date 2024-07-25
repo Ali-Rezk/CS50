065c24ae8a0ebb6d9b9472ebb07aeebf3e8f732b
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-25 18:36:37 +0000

    Thu, Jul 25, 2024, 9:36 PM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -29,7 +29,7 @@ def index():
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
         if not name:
-            redirect("/")
+            return redirect("/")
         month = request.form.get("month")
         if not month:
             redirect("/")
