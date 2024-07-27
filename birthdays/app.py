0dbd1f959211780d01f92af30768374953c07721
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-27 01:18:58 +0000

    Sat, Jul 27, 2024, 4:18 AM +03:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -32,7 +32,7 @@ def index():
         day = request.form.get("day")
         if not name or not month or not day:
             return redirect("/")
-        if month not in range(1,13):
+        elif month not in range(1,13):
             return redirect("/")
         db.execute("INSERT INTO birthdays (name,month,day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
