28e3f747bdb6de8ef39c48f3c4884288311b309a
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-28 21:46:01 +0000

    Mon, Jul 29, 2024, 12:46 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -60,7 +60,6 @@ def buy():
         row = db.execute("SELECT cash FROM users WHERE id = (?)", user_id)
         cash = row[0]["cash"]
         cash = int(cash) - int(price)
-        print(cash, price)
         if int(cash) < 0:
             return apology("Sorry not enough cash")
         db.execute("INSERT INTO stocks (stocks_id, symbol, shares) VALUES (?, ?, ?)", user_id, symbol, shares)
