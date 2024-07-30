0fcbae252aaa26632f3c58360af92108927fe5b3
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-30 21:00:37 +0000

    Wed, Jul 31, 2024, 12:00 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -71,8 +71,8 @@ def buy():
         if int(cash) < 0:
             return apology("Sorry not enough cash")
         old_shares = db.execute("SELECT shares FROM stocks WHERE stocks_id = ? AND symbol = ?", user_id, symbol)
-        new_shares = int(shares) + int(old_shares)
         try:
+            new_shares = int(shares) + int(old_shares[0]["shares"])
             db.execute("UPDATE stocks SET shares = ? WHERE symbol = ? AND stocks_id = ?", new_shares, symbol, user_id)
         except:
             db.execute("INSERT INTO stocks (stocks_id, symbol, shares) VALUES (?, ?, ?)", user_id, symbol, shares)
