b609eee44d397fd52f62c8632602f59151eb1107
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-07-30 21:23:36 +0000

    Wed, Jul 31, 2024, 12:23 AM +03:00

--- finance/app.py
+++ finance/app.py
@@ -86,8 +86,7 @@ def buy():
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
-
+    
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
