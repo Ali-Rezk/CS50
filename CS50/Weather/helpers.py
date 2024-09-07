5d54fa522330f5ee339acf382e4e27ffd1611b48
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-07 20:01:23 +0000

    Sat, Sep 7, 2024, 11:01 PM +03:00

--- CS50/Weather/helpers.py
+++ CS50/Weather/helpers.py
@@ -49,20 +49,19 @@ def login_required(f):
     return decorated_function
 
 
+def lookup(user_input):
+    """open weather api"""
+    api_key = '893cbbac726aa5674136bdefde03d5b5'
 
-api_key = '893cbbac726aa5674136bdefde03d5b5'
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
 
-user_input = input("Enter city: ")
+    if weather_data.json()['cod'] == '404':
+        print("No City Found")
+    else:
+        weather = weather_data.json()['weather'][0]['main']
+        temp = round(weather_data.json()['main']['temp'])
 
-weather_data = requests.get(
-    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
-
-if weather_data.json()['cod'] == '404':
-    print("No City Found")
-else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
-
-    print(f"The weather in {user_input} is: {weather}")
-    print(f"The temperature in {user_input} is: {temp}ºF")
-    print(weather_data.json())
+        print(f"The weather in {user_input} is: {weather}")
+        print(f"The temperature in {user_input} is: {temp}ºF")
+        print(weather_data.json())
