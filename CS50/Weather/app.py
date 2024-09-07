279b20b5dc0f8a59ccee2ed49c78c1f668f6c03f
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-07 20:56:43 +0000

    Sat, Sep 7, 2024, 11:56 PM +03:00

--- CS50/Weather/app.py
+++ CS50/Weather/app.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required, lookup
 
 app = Flask(__name__)
 
