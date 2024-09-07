d7c886a7a9b00b3c8e6ebf8dfa6beb9f9f32b345
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-07 20:03:36 +0000

    Sat, Sep 7, 2024, 11:03 PM +03:00

--- CS50/Weather/helpers.py
+++ CS50/Weather/helpers.py
@@ -57,11 +57,12 @@ def lookup(user_input):
         f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
 
     if weather_data.json()['cod'] == '404':
-        print("No City Found")
+        not_found = "No City Found"
     else:
         weather = weather_data.json()['weather'][0]['main']
         temp = round(weather_data.json()['main']['temp'])
 
+        return weather, temp
         print(f"The weather in {user_input} is: {weather}")
         print(f"The temperature in {user_input} is: {temp}ºF")
         print(weather_data.json())
