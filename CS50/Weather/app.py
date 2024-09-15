71695639b859136a161f56a5577781d1b91d30da
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-15 02:00:37 +0000

    Sun, Sep 15, 2024, 5:00 AM +03:00

--- CS50/Weather/app.py
+++ CS50/Weather/app.py
@@ -54,8 +54,6 @@ def index():
     condition = lookedup[2][0]['day']['condition']['text']
     icon = lookedup[2][0]['day']['condition']['icon']
     daily = [round(max_temp), round(avg_temp), round(min_temp), rain_chance, snow_chance, condition, icon]
-    print(current)
-    print(daily)
 
     sunrise = lookedup[4]['sunrise']
     sunset = lookedup[4]['sunset']
@@ -63,60 +61,37 @@ def index():
     moonset = lookedup[4]['moonset']
     moon_phase = lookedup[4]['moon_phase']
     moon_illumination = lookedup[4]['moon_illumination']
-    astra = [sunrise,sunset,moonrise,moonset,moon_phase,moon_illumination]
+    astro = [sunrise,sunset,moonrise,moonset,moon_phase,moon_illumination]
 
     now = datetime.now().strftime("%Y-%m-%d %H:%M")
     date_time = now.split()
 
-    return render_template("today.html", location=location, current=current, date_time=date_time, astra=astra, daily=daily)
+    return render_template("today.html", location=location, current=current, date_time=date_time, astro=astro, daily=daily)
 
 @app.route("/hourly")
 def hourly():
-    lookedup = lookup("cairo","","")
+    lookedup = lookup("cairo","5","")
 
     city = lookedup[0]['name']
     region = lookedup[0]['region']
     country = lookedup[0]['country']
     tz_id = lookedup[0]['tz_id']
-
     location = [city, region, country]
 
-    temp = lookedup[1]['temp_c']
-    condition = lookedup[1]['condition']['text']
-    wind_speed = lookedup[1]['wind_kph']
-    wind_dir = lookedup[1]['wind_dir']
-    pressure_mb = lookedup[1]['pressure_mb']
-    humidity = lookedup[1]['humidity']
-    feelslike_c = lookedup[1]['feelslike_c']
-    vis_km = lookedup[1]['vis_km']
-    uv = lookedup[1]['uv']
-    dewpoint = lookedup[1]['dewpoint_c']
-    icon = lookedup[1]['condition']['icon']
-    current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, dewpoint, icon]
 
-    max_temp = lookedup[2][0]['day']['maxtemp_c']
-    avg_temp = lookedup[2][0]['day']['avgtemp_c']
-    min_temp = lookedup[2][0]['day']['mintemp_c']
-    rain_chance = lookedup[2][0]['day']['daily_chance_of_rain']
-    snow_chance = lookedup[2][0]['day']['daily_chance_of_snow']
-    condition = lookedup[2][0]['day']['condition']['text']
-    icon = lookedup[2][0]['day']['condition']['icon']
-    daily = [round(max_temp), round(avg_temp), round(min_temp), rain_chance, snow_chance, condition, icon]
+    hourly = lookedup[2]
 
-    hourly = lookedup[3]
+    for i in range(0,5):
+        ss = hourly[i]['date'].split("-")
+        date = datetime(int(ss[0]), int(ss[1]), int(ss[2]))
+        hourly[i]['date'] = date.strftime("%A,%B %d")
 
-    for i in range(0,24):
-        n = hourly[i]
-        n['time'] = n['time'].split()[1]
-        hourly[i] = n
+        for x in range(0,24):
+            n = hourly[i]['hour'][x]
+            n['time'] = n['time'].split()[1]
+            hourly[i]['hour'][x] = n
 
-    sunrise = lookedup[4]['sunrise']
-    sunset = lookedup[4]['sunset']
-    moonrise = lookedup[4]['moonrise']
-    moonset = lookedup[4]['moonset']
-    moon_phase = lookedup[4]['moon_phase']
-    moon_illumination = lookedup[4]['moon_illumination']
-    astro = [sunrise,sunset,moonrise,moonset,moon_phase,moon_illumination,]
+    print(hourly)
 
     timezone = pytz.timezone(tz_id)
     now = datetime.now(timezone).strftime('%Y-%m-%d %H:%M %Z %z')
@@ -125,4 +100,41 @@ def hourly():
     date_time[3] = ss[1]
 
 
-    return render_template("hourly.html", location=location, current=current, date_time=date_time, astro=astro, daily=daily, hourly=hourly)
+    return render_template("hourly.html", location=location, date_time=date_time, hourly=hourly)
+
+
+@app.route("/daily")
+def daily():
+    lookedup = lookup("cairo","5","")
+
+    city = lookedup[0]['name']
+    region = lookedup[0]['region']
+    country = lookedup[0]['country']
+    tz_id = lookedup[0]['tz_id']
+    location = [city, region, country]
+
+
+    hourly = lookedup[2]
+
+    for i in range(0,5):
+        ss = hourly[i]['date'].split("-")
+        date = datetime(int(ss[0]), int(ss[1]), int(ss[2]))
+        hourly[i]['date'] = date.strftime("%A,%B %d")
+        hourly[i]['date_day'] = date.strftime("%A, %d")
+        hourly[i]['day_day'] = date.strftime("%d")
+
+        for x in range(0,24):
+            n = hourly[i]['hour'][x]
+            n['time'] = n['time'].split()[1]
+            hourly[i]['hour'][x] = n
+
+
+    timezone = pytz.timezone(tz_id)
+    now = datetime.now(timezone).strftime('%Y-%m-%d %H:%M %Z %z')
+    date_time = now.split()
+    ss = str(datetime.now(timezone)).split('+')
+    date_time[3] = ss[1]
+
+
+    return render_template("daily.html", location=location, date_time=date_time, hourly=hourly)
+
