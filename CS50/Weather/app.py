1b8d195363b929bd3c531452571b6ea08b4656df
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-18 00:50:00 +0000

    Wed, Sep 18, 2024, 3:50 AM +03:00

--- CS50/Weather/app.py
+++ CS50/Weather/app.py
@@ -1,9 +1,12 @@
 from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 from helpers import apology, login_required, lookup
+import geocoder
+import time
+from flask_cors import CORS
 import pytz
 
 app = Flask(__name__)
@@ -23,54 +26,68 @@ def after_request(response):
     return response
 
 
-@app.route("/")
+@app.route("/",methods =["POST","GET"])
 def index():
-    lookedup = lookup("cairo","0","0")
 
-    city = lookedup[0]['name']
-    region = lookedup[0]['region']
-    country = lookedup[0]['country']
+    print(request.method)
+    if request.method == "POST":
+        name = request.form.get("search")
 
-    location = [city, region, country]
+        lookedup = lookup("cairo")
 
-    temp = lookedup[1]['temp_c']
-    condition = lookedup[1]['condition']['text']
-    wind_speed = lookedup[1]['wind_kph']
-    wind_dir = lookedup[1]['wind_dir']
-    pressure_mb = lookedup[1]['pressure_mb']
-    humidity = lookedup[1]['humidity']
-    feelslike_c = lookedup[1]['feelslike_c']
-    vis_km = lookedup[1]['vis_km']
-    uv = lookedup[1]['uv']
-    dewpoint = lookedup[1]['dewpoint_c']
-    icon = lookedup[1]['condition']['icon']
-    current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, dewpoint, icon]
+        city = lookedup[0]['name']
+        region = lookedup[0]['region']
+        country = lookedup[0]['country']
 
-    max_temp = lookedup[2][0]['day']['maxtemp_c']
-    avg_temp = lookedup[2][0]['day']['avgtemp_c']
-    min_temp = lookedup[2][0]['day']['mintemp_c']
-    rain_chance = lookedup[2][0]['day']['daily_chance_of_rain']
-    snow_chance = lookedup[2][0]['day']['daily_chance_of_snow']
-    condition = lookedup[2][0]['day']['condition']['text']
-    icon = lookedup[2][0]['day']['condition']['icon']
-    daily = [round(max_temp), round(avg_temp), round(min_temp), rain_chance, snow_chance, condition, icon]
+        location = [city, region, country]
 
-    sunrise = lookedup[4]['sunrise']
-    sunset = lookedup[4]['sunset']
-    moonrise = lookedup[4]['moonrise']
-    moonset = lookedup[4]['moonset']
-    moon_phase = lookedup[4]['moon_phase']
-    moon_illumination = lookedup[4]['moon_illumination']
-    astro = [sunrise,sunset,moonrise,moonset,moon_phase,moon_illumination]
+        temp = lookedup[1]['temp_c']
+        condition = lookedup[1]['condition']['text']
+        wind_speed = lookedup[1]['wind_kph']
+        wind_dir = lookedup[1]['wind_dir']
+        pressure_mb = lookedup[1]['pressure_mb']
+        humidity = lookedup[1]['humidity']
+        feelslike_c = lookedup[1]['feelslike_c']
+        vis_km = lookedup[1]['vis_km']
+        uv = lookedup[1]['uv']
+        dewpoint = lookedup[1]['dewpoint_c']
+        icon = lookedup[1]['condition']['icon']
+        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, dewpoint, icon]
 
-    now = datetime.now().strftime("%Y-%m-%d %H:%M")
-    date_time = now.split()
+        max_temp = lookedup[2][0]['day']['maxtemp_c']
+        avg_temp = lookedup[2][0]['day']['avgtemp_c']
+        min_temp = lookedup[2][0]['day']['mintemp_c']
+        rain_chance = lookedup[2][0]['day']['daily_chance_of_rain']
+        snow_chance = lookedup[2][0]['day']['daily_chance_of_snow']
+        condition = lookedup[2][0]['day']['condition']['text']
+        icon = lookedup[2][0]['day']['condition']['icon']
+        daily = [round(max_temp), round(avg_temp), round(min_temp), rain_chance, snow_chance, condition, icon]
 
-    return render_template("today.html", location=location, current=current, date_time=date_time, astro=astro, daily=daily)
+        sunrise = lookedup[4]['sunrise']
+        sunset = lookedup[4]['sunset']
+        moonrise = lookedup[4]['moonrise']
+        moonset = lookedup[4]['moonset']
+        moon_phase = lookedup[4]['moon_phase']
+        moon_illumination = lookedup[4]['moon_illumination']
+        astro = [sunrise,sunset,moonrise,moonset,moon_phase,moon_illumination]
+
+        now = datetime.now().strftime("%Y-%m-%d %H:%M")
+        date_time = now.split()
+
+        return redirect("today.html", loading = False,location=location, current=current, date_time=date_time, astro=astro, daily=daily)
+    else:
+        data = request.args
+
+        loading = True
+
+
+        # return render_template("today.html")
+        return render_template("today.html", loading=loading)
+
 
 @app.route("/hourly")
 def hourly():
-    lookedup = lookup("cairo","5","")
+    lookedup = lookup("cairo")
 
     city = lookedup[0]['name']
     region = lookedup[0]['region']
@@ -105,7 +122,7 @@ def hourly():
 
 @app.route("/daily")
 def daily():
-    lookedup = lookup("cairo","5","")
+    lookedup = lookup("cairo")
 
     city = lookedup[0]['name']
     region = lookedup[0]['region']
@@ -137,4 +154,12 @@ def daily():
 
 
     return render_template("daily.html", location=location, date_time=date_time, hourly=hourly)
+
+
+@app.route("/history")
+def history():
+    data = request.args
+    print(data)
+
+    return render_template("history.html")
 
--- CS50/Weather/helpers.py
+++ CS50/Weather/helpers.py
@@ -49,29 +49,49 @@ def login_required(f):
     return decorated_function
 
 
-def lookup(country, days, hour):
-
+def lookup(country, lng=0):
     api_key = '5aba5ac55b984050bfa230233240709'
+    days = 5
 
-    currentWeather_data = requests.get(
+    try:
+        int(country)
+        lat = country
+        long = lng
+        currentWeather_data = requests.get(
+        f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={lat},{long}&aqi=no"
+        )
+
+        daily_data = requests.get(
+            f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={lat},{long}&days={days}&aqi=yes&alerts=no"
+        )
+    except:
+        user_input = country
+        currentWeather_data = requests.get(
         f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={country}&aqi=no"
-    )
+        )
 
-    daily_data = requests.get(
+        daily_data = requests.get(
         f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={country}&days={days}&aqi=yes&alerts=no"
-    )
+        )
 
-    hourly_data = requests.get(
-        f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={country}&days={days}&aqi=yes&alerts=no&hour={hour}"
-    )
+
+
+
+    # hourly_data = requests.get(
+    #     f"https://api.weatherapi.com/v1/forecast.json?key={api_key}&q={country}&days={days}&aqi=yes&alerts=no&hour={hour}"
+    # )
+
+    # history = requests.get(
+    #     f"https://api.weatherapi.com/v1/history.json?key={api_key}&q={country}&dt={date}"
+    # )
 
     try:
         # date, max/avg/min temp, avg wind speed, avg humidity, rain chance, snow chance, condition, icon
         daily = daily_data.json()['forecast']['forecastday']
         # time, temp, is_day?, condition, wind speed/dir, humidity, feelslike, vis, uv?, gust?, snow_cm?, icon, cloud cover, pressure, dew_point
-        hourly = hourly_data.json()['forecast']['forecastday'][0]['hour']
+        hourly = daily_data.json()['forecast']['forecastday'][0]['hour']
         # sunrise, sunset, moon_phase, moon_illumination, moonset, moonrise
-        astro = hourly_data.json()['forecast']['forecastday'][0]['astro']
+        astro = daily_data.json()['forecast']['forecastday'][0]['astro']
         # name, region, country, localtime
         location = currentWeather_data.json()['location']
         # temp, is_day?, condition, wind speed/dir, humidity, feelslike, vis, uv?, gust?, snow_cm?, icon, dewpoint
