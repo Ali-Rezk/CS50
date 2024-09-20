b5f93a2f20819c61e2c31cf65a021412938efd85
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-20 01:15:19 +0000

    Fri, Sep 20, 2024, 4:15 AM +03:00

--- CS50/Weather/app.py
+++ CS50/Weather/app.py
@@ -18,92 +18,18 @@ app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///weather.db")
-
-# @app.after_request
-# def after_request(response):
-#     """Ensure responses aren't cached"""
-#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-#     response.headers["Expires"] = 0
-#     response.headers["Pragma"] = "no-cache"
-#     return response
-
-
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     """Log user in"""
-
-#     # Forget any user_id
-#     session.clear()
-
-#     # User reached route via POST (as by submitting a form via POST)
-#     if request.method == "POST":
-#         # Ensure username was submitted
-#         if not request.form.get("username"):
-#             return apology("must provide username", 403)
-
-#         # Ensure password was submitted
-#         elif not request.form.get("password"):
-#             return apology("must provide password", 403)
-
-#         # Query database for username
-#         rows = db.execute(
-#             "SELECT * FROM users WHERE username = ?", request.form.get("username")
-#         )
-
-#         # Ensure username exists and password is correct
-#         if len(rows) != 1 or not check_password_hash(
-#             rows[0]["hash"], request.form.get("password")
-#         ):
-#             return apology("invalid username and/or password", 403)
-
-#         # Remember which user has logged in
-#         session["user_id"] = rows[0]["id"]
-
-#         # Redirect user to home page
-#         return redirect("/")
-
-#     # User reached route via GET (as by clicking a link or via redirect)
-#     else:
-#         return render_template("login.html")
-
-# @app.route("/register", methods=["GET", "POST"])
-# def register():
-#     """Register user"""
-#     if request.method == "POST":
-#         reg_username = request.form.get("username")
-#         reg_password = request.form.get("password")
-#         confirmation = request.form.get("confirmation")
-#         if not reg_username:
-#             return apology("must provide username", 400)
-#         elif not reg_password:
-#             return apology("must provide password", 400)
-#         elif not confirmation:
-#             return apology("must provide confirmation", 400)
-#         elif confirmation != reg_password:
-#             return apology("password and confirmation does not match", 400)
-#         hash_password = generate_password_hash(reg_password, method='pbkdf2', salt_length=16)
-#         try:
-#             db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
-#                        reg_username, hash_password)
-#         except ValueError:
-#             return apology("username is taken")
-#         return render_template("login.html")
-
-#     else:
-#         return render_template("register.html")
 
 @app.route("/",methods =["POST","GET"])
 def index():
     print(request.method)
     print(session)
-    session["coords"] = [None, None]
-    session["city"] = None
     if request.method == "POST":
         name = request.form.get("search")
-        data = request.get_json()
-        session['coords'] = [data['latitude'], data['longitude']]
+        try:
+            data = request.get_json()
+            session['coords'] = [data['latitude'], data['longitude']]
+        except:
+            print("error")
         print(session)
         session["city"] = name
         lookedup = lookup(name)
@@ -147,9 +73,10 @@ def index():
         now = datetime.now().strftime("%Y-%m-%d %H:%M")
         date_time = now.split()
 
-        return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro, daily=daily)
+        return redirect("today")
     else:
         session["coords"] = [None, None]
+        session["city"] = None
         if isinstance(session['coords'][0], float):
             print("float")
         else:
@@ -197,11 +124,16 @@ def index():
 
 @app.route("/today",methods =["POST","GET"])
 def today():
-
     print(request.method)
     print(session)
     if request.method == "POST":
         name = request.form.get("search")
+
+        try:
+            data = request.get_json()
+            session['coords'] = [data['latitude'], data['longitude']]
+        except:
+            print("error")
 
         if name:
             session["city"] = name
@@ -255,14 +187,15 @@ def today():
         return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro, daily=daily)
 
     else:
-        print(session)
         if session["city"] is not None:
             name = session["city"]
-        elif session["coords"] is not None:
+        elif session["coords"][0] is not None:
             name = session["coords"]
         else:
-            return redirect("temp.html")
+            location_missing = True
+            return apology("today.html", location_missing ,"Please allow location access or use the search bar")
         print(name)
+        print("TEST")
         lookedup = lookup(name)
 
         city = lookedup[0]['name']
@@ -303,8 +236,6 @@ def today():
 
         now = datetime.now().strftime("%Y-%m-%d %H:%M")
         date_time = now.split()
-        print(session["coords"])
-        print(session["city"])
 
         return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro, daily=daily)
 
@@ -315,6 +246,20 @@ def hourly():
     print(session)
     if request.method == "POST":
         name = request.form.get("search")
+
+        try:
+            data = request.get_json()
+            session['coords'] = [data['latitude'], data['longitude']]
+        except:
+            print("error")
+
+        if name:
+            session["city"] = name
+            print(name)
+        elif session["coords"]:
+            name = session["coords"]
+        else:
+            name = session["city"]
 
         session["city"] = name
         lookedup = lookup(name)
@@ -349,12 +294,13 @@ def hourly():
         return render_template("hourly.html", location=location, date_time=date_time, hourly=hourly)
 
     else:
-        print(session)
-
         if session["city"] is not None:
             name = session["city"]
-        elif session["coords"]:
+        elif session["coords"][0] is not None:
             name = session["coords"]
+        else:
+            location_missing = True
+            return apology("hourly.html", location_missing ,"Please allow location access or use the search bar")
 
         lookedup = lookup(name)
 
@@ -396,6 +342,12 @@ def daily():
     if request.method == "POST":
         name = request.form.get("search")
 
+        try:
+            data = request.get_json()
+            session['coords'] = [data['latitude'], data['longitude']]
+        except:
+            print("error")
+
         lookedup = lookup(name)
         session["city"] = name
 
@@ -431,12 +383,13 @@ def daily():
         return render_template("daily.html", location=location, date_time=date_time, hourly=hourly)
 
     else:
-        print(session["coords"])
-
         if session["city"] is not None:
             name = session["city"]
-        elif session["coords"]:
+        elif session["coords"][0] is not None:
             name = session["coords"]
+        else:
+            location_missing = True
+            return apology("daily.html", location_missing ,"Please allow location access or use the search bar")
 
         lookedup = lookup(name)
 
