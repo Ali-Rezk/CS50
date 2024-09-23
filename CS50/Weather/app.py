8cf059df4bbe093b77e4db452c14b744e16318aa
Ali-Rezk <156038370+Ali-Rezk@users.noreply.github.com>
2024-09-23 05:36:56 +0000

    Mon, Sep 23, 2024, 8:36 AM +03:00

--- CS50/Weather/app.py
+++ CS50/Weather/app.py
@@ -19,6 +19,14 @@ app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.before_request
+def initialize_session():
+    if 'coords' not in session:
+        session['coords'] = [None, None]
+    if 'city' not in session:
+        session['city'] = None
+    if 'unit' not in session:
+        session['unit'] = None
 
 @app.route("/",methods =["POST","GET"])
 def index():
@@ -26,9 +34,12 @@ def index():
     print(session)
     if request.method == "POST":
         name = request.form.get("search")
+        symbol = request.form.get("symbol")
+        change_unit = True
         try:
             data = request.get_json()
             session['coords'] = [data['latitude'], data['longitude']]
+            change_unit = False
         except:
             print("error")
         print(session)
@@ -53,6 +64,28 @@ def index():
 
         location = [city, region, country]
 
+        if change_unit:
+            if symbol == "F°":
+                temp = lookedup[1]['temp_f']
+                feelslike_c = lookedup[1]['feelslike_f']
+                dewpoint = lookedup[1]['dewpoint_f']
+                max_temp = lookedup[2][0]['day']['maxtemp_f']
+                avg_temp = lookedup[2][0]['day']['avgtemp_f']
+                min_temp = lookedup[2][0]['day']['mintemp_f']
+                session["unit"] = "F°"
+                unit1 = "F°"
+                unit2 = "C°"
+            else:
+                temp = lookedup[1]['temp_c']
+                feelslike_c = lookedup[1]['feelslike_c']
+                dewpoint = lookedup[1]['dewpoint_c']
+                max_temp = lookedup[2][0]['day']['maxtemp_c']
+                avg_temp = lookedup[2][0]['day']['avgtemp_c']
+                min_temp = lookedup[2][0]['day']['mintemp_c']
+                session["unit"] = "C°"
+                unit1 = "C°"
+                unit2 = "F°"
+
         temp = lookedup[1]['temp_c']
         condition = lookedup[1]['condition']['text']
         wind_speed = lookedup[1]['wind_kph']
@@ -65,7 +98,7 @@ def index():
         dewpoint = lookedup[1]['dewpoint_c']
         icon = lookedup[1]['condition']['icon']
         is_day = lookedup[1]['is_day']
-        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, dewpoint, icon, is_day]
+        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, round(dewpoint), icon, is_day]
 
         max_temp = lookedup[2][0]['day']['maxtemp_c']
         avg_temp = lookedup[2][0]['day']['avgtemp_c']
@@ -91,11 +124,19 @@ def index():
     else:
         session["coords"] = [None, None]
         session["city"] = None
+        session["unit"] = None
         city = "-"
         region = "-"
         country = "-"
 
         location = [city, region, country]
+
+        if session["unit"] == "F°":
+            unit1 = "F°"
+            unit2 = "C°"
+        else:
+            unit1 = "C°"
+            unit2 = "F°"
 
         temp = "-"
         condition = "-"
@@ -129,7 +170,8 @@ def index():
 
         now = datetime.now().strftime("%Y-%m-%d %H:%M")
         date_time = now.split()
-        return render_template("temp.html",location=location, current=current, date_time=date_time, astro=astro, daily=daily)
+        return render_template("temp.html",location=location, current=current, date_time=date_time, astro=astro,
+                                    daily=daily, unit1=unit1, unit2=unit2)
 
 
 @app.route("/today",methods =["POST","GET"])
@@ -139,52 +181,64 @@ def today():
     if request.method == "POST":
         name = request.form.get("search")
         symbol = request.form.get("symbol")
-        print(symbol)
         # session['data'] = search(name)
+        change_unit = True
         try:
             data = request.get_json()
             print(data)
             session['coords'] = [data['latitude'], data['longitude']]
+            change_unit = False
         except:
             print("error")
 
         if name:
             session["city"] = name
+            print("name")
         elif session["city"] is not None:
             name = session["city"]
+            print("city")
         elif session["coords"][0] is not None:
             name = session["coords"]
+            print("coords")
         else:
             location_missing = True
             return apology("today.html", location_missing ,"Please allow location access or use the search bar")
-        print(name)
 
         lookedup = lookup(name)
         if lookedup == "No City Found":
             location_missing = True
             return apology("today.html", location_missing ,"City not found")
-
+        print(session["unit"])
         city = lookedup[0]['name']
         region = lookedup[0]['region']
         country = lookedup[0]['country']
         tz_id = lookedup[0]['tz_id']
         location = [city, region, country]
 
-        if symbol == "F°":
-            temp = lookedup[1]['temp_f']
-            feelslike_c = lookedup[1]['feelslike_f']
-            dewpoint = lookedup[1]['dewpoint_f']
-            max_temp = lookedup[2][0]['day']['maxtemp_f']
-            avg_temp = lookedup[2][0]['day']['avgtemp_f']
-            min_temp = lookedup[2][0]['day']['mintemp_f']
-        else:
-            temp = lookedup[1]['temp_c']
-            feelslike_c = lookedup[1]['feelslike_c']
-            dewpoint = lookedup[1]['dewpoint_c']
-            max_temp = lookedup[2][0]['day']['maxtemp_c']
-            avg_temp = lookedup[2][0]['day']['avgtemp_c']
-            min_temp = lookedup[2][0]['day']['mintemp_c']
-
+        if change_unit:
+            if symbol == "F°":
+                temp = lookedup[1]['temp_f']
+                feelslike_c = lookedup[1]['feelslike_f']
+                dewpoint = lookedup[1]['dewpoint_f']
+                max_temp = lookedup[2][0]['day']['maxtemp_f']
+                avg_temp = lookedup[2][0]['day']['avgtemp_f']
+                min_temp = lookedup[2][0]['day']['mintemp_f']
+                session["unit"] = "F°"
+                unit1 = "F°"
+                unit2 = "C°"
+            else:
+                temp = lookedup[1]['temp_c']
+                feelslike_c = lookedup[1]['feelslike_c']
+                dewpoint = lookedup[1]['dewpoint_c']
+                max_temp = lookedup[2][0]['day']['maxtemp_c']
+                avg_temp = lookedup[2][0]['day']['avgtemp_c']
+                min_temp = lookedup[2][0]['day']['mintemp_c']
+                session["unit"] = "C°"
+                unit1 = "C°"
+                unit2 = "F°"
+        print(session["unit"])
+        print(symbol)
+        print("test")
         condition = lookedup[1]['condition']['text']
         wind_speed = lookedup[1]['wind_kph']
         wind_dir = lookedup[1]['wind_dir']
@@ -196,7 +250,7 @@ def today():
 
         icon = lookedup[1]['condition']['icon']
         is_day = lookedup[1]['is_day']
-        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, dewpoint, icon, is_day]
+        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, round(dewpoint), icon, is_day]
 
         rain_chance = lookedup[2][0]['day']['daily_chance_of_rain']
         snow_chance = lookedup[2][0]['day']['daily_chance_of_snow']
@@ -218,11 +272,10 @@ def today():
         match = re.search(r'([+-]\d{2}:\d{2})$', str(datetime.now(timezone)))
         date_time[4] = match.group(1)
 
-        return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro, daily=daily)
+        return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro,
+                                    daily=daily, unit1=unit1, unit2=unit2)
 
     else:
-        symbol = request.form.get("symbol")
-        print(symbol)
         if session["city"] is not None:
             name = session["city"]
         elif session["coords"][0] is not None:
@@ -230,8 +283,7 @@ def today():
         else:
             location_missing = True
             return apology("today.html", location_missing ,"Please allow location access or use the search bar")
-        print(name)
-        print("TEST")
+
         lookedup = lookup(name)
 
         if lookedup == "No City Found":
@@ -244,13 +296,18 @@ def today():
         tz_id = lookedup[0]['tz_id']
         location = [city, region, country]
 
-        if symbol == "F°":
+        print(session["unit"])
+        print("test")
+
+        if session["unit"] == "F°":
             temp = lookedup[1]['temp_f']
             feelslike_c = lookedup[1]['feelslike_f']
             dewpoint = lookedup[1]['dewpoint_f']
             max_temp = lookedup[2][0]['day']['maxtemp_f']
             avg_temp = lookedup[2][0]['day']['avgtemp_f']
             min_temp = lookedup[2][0]['day']['mintemp_f']
+            unit1 = "F°"
+            unit2 = "C°"
         else:
             temp = lookedup[1]['temp_c']
             feelslike_c = lookedup[1]['feelslike_c']
@@ -258,6 +315,10 @@ def today():
             max_temp = lookedup[2][0]['day']['maxtemp_c']
             avg_temp = lookedup[2][0]['day']['avgtemp_c']
             min_temp = lookedup[2][0]['day']['mintemp_c']
+            session["unit"] == "C°"
+            unit1 = "C°"
+            unit2 = "F°"
+
 
         condition = lookedup[1]['condition']['text']
         wind_speed = lookedup[1]['wind_kph']
@@ -268,7 +329,7 @@ def today():
         uv = lookedup[1]['uv']
         icon = lookedup[1]['condition']['icon']
         is_day = lookedup[1]['is_day']
-        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, dewpoint, icon, is_day]
+        current = [round(temp), condition, round(feelslike_c), wind_speed, wind_dir, pressure_mb, humidity, vis_km, uv, round(dewpoint), icon, is_day]
 
         rain_chance = lookedup[2][0]['day']['daily_chance_of_rain']
         snow_chance = lookedup[2][0]['day']['daily_chance_of_snow']
@@ -290,7 +351,8 @@ def today():
         match = re.search(r'([+-]\d{2}:\d{2})$', str(datetime.now(timezone)))
         date_time[4] = match.group(1)
 
-        return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro, daily=daily)
+        return render_template("today.html",location=location, current=current, date_time=date_time, astro=astro,
+                                     daily=daily, unit1=unit1, unit2=unit2)
 
 
 @app.route("/hourly", methods = ["POST","GET"])
@@ -299,10 +361,12 @@ def hourly():
     print(session)
     if request.method == "POST":
         name = request.form.get("search")
-
+        symbol = request.form.get("symbol")
+        change_unit = True
         try:
             data = request.get_json()
             session['coords'] = [data['latitude'], data['longitude']]
+            change_unit = False
         except:
             print("error")
 
@@ -331,15 +395,27 @@ def hourly():
 
         hourly = lookedup[2]
 
-        for i in range(0,5):
-            ss = hourly[i]['date'].split("-")
+        if change_unit:
+            if symbol == "F°":
+                session["unit"] = "F°"
+                unit1 = "F°"
+                unit2 = "C°"
+                unit = "f"
+            else:
+                session["unit"] = "C°"
+                unit1 = "C°"
+                unit2 = "F°"
+                unit = "c"
+
+        for day in hourly:
+            ss = day['date'].split("-")
             date = datetime(int(ss[0]), int(ss[1]), int(ss[2]))
-            hourly[i]['date'] = date.strftime("%A,%B %d")
+            day['date'] = date.strftime("%A,%B %d")
 
             for x in range(0,24):
-                n = hourly[i]['hour'][x]
+                n = day['hour'][x]
                 n['time'] = n['time'].split()[1]
-                hourly[i]['hour'][x] = n
+                day['hour'][x] = n
 
         timezone = pytz.timezone(tz_id)
         now = datetime.now(timezone).strftime('%Y-%m-%d %I:%M %p %Z %z')
@@ -348,7 +424,7 @@ def hourly():
         date_time[4] = match.group(1)
 
 
-        return render_template("hourly.html", location=location, date_time=date_time, hourly=hourly)
+        return render_template("hourly.html", location=location, date_time=date_time, hourly=hourly, unit=unit ,unit1=unit1, unit2=unit2)
 
     else:
         if session["city"] is not None:
@@ -371,6 +447,14 @@ def hourly():
         tz_id = lookedup[0]['tz_id']
         location = [city, region, country]
 
+        if session["unit"] == "F°":
+            unit1 = "F°"
+            unit2 = "C°"
+            unit = "f"
+        else:
+            unit1 = "C°"
+            unit2 = "F°"
+            unit = "c"
 
         hourly = lookedup[2]
         for day in hourly:
@@ -390,7 +474,7 @@ def hourly():
         match = re.search(r'([+-]\d{2}:\d{2})$', str(datetime.now(timezone)))
         date_time[4] = match.group(1)
 
-        return render_template("hourly.html", location=location, date_time=date_time, hourly=hourly)
+        return render_template("hourly.html", location=location, date_time=date_time, hourly=hourly, unit=unit ,unit1=unit1, unit2=unit2)
 
 
 @app.route("/daily", methods = ["POST", "GET"])
@@ -399,10 +483,13 @@ def daily():
     print(session)
     if request.method == "POST":
         name = request.form.get("search")
+        symbol = request.form.get("symbol")
 
+        change_unit = True
         try:
             data = request.get_json()
             session['coords'] = [data['latitude'], data['longitude']]
+            change_unit = False
         except:
             print("error")
 
@@ -428,21 +515,31 @@ def daily():
         tz_id = lookedup[0]['tz_id']
         location = [city, region, country]
 
+        if change_unit:
+            if symbol == "F°":
+                session["unit"] = "F°"
+                unit1 = "F°"
+                unit2 = "C°"
+                unit = "f"
+            else:
+                session["unit"] = "C°"
+                unit1 = "C°"
+                unit2 = "F°"
+            unit = "c"
 
-        hourly = lookedup[2]
+        daily = lookedup[2]
 
-        for i in range(0,5):
-            ss = hourly[i]['date'].split("-")
+        for day in daily:
+            ss = day['date'].split("-")
             date = datetime(int(ss[0]), int(ss[1]), int(ss[2]))
-            hourly[i]['date'] = date.strftime("%A,%B %d")
-            hourly[i]['date_day'] = date.strftime("%A, %d")
-            hourly[i]['day_day'] = date.strftime("%d")
+            day['date'] = date.strftime("%A,%B %d")
+            day['date_day'] = date.strftime("%A, %d")
+            day['day_day'] = date.strftime("%d")
 
             for x in range(0,24):
-                n = hourly[i]['hour'][x]
+                n = day['hour'][x]
                 n['time'] = n['time'].split()[1]
-                hourly[i]['hour'][x] = n
-
+                day['hour'][x] = n
 
         timezone = pytz.timezone(tz_id)
         now = datetime.now(timezone).strftime('%Y-%m-%d %I:%M %p %Z %z')
@@ -451,7 +548,7 @@ def daily():
         date_time[4] = match.group(1)
 
 
-        return render_template("daily.html", location=location, date_time=date_time, hourly=hourly)
+        return render_template("daily.html", location=location, date_time=date_time, daily=daily, unit=unit ,unit1=unit1, unit2=unit2)
 
     else:
         if session["city"] is not None:
@@ -474,10 +571,18 @@ def daily():
         tz_id = lookedup[0]['tz_id']
         location = [city, region, country]
 
+        if session["unit"] == "F°":
+            unit1 = "F°"
+            unit2 = "C°"
+            unit = "f"
+        else:
+            unit1 = "C°"
+            unit2 = "F°"
+            unit = "c"
 
-        hourly = lookedup[2]
+        daily = lookedup[2]
 
-        for day in hourly:
+        for day in daily:
             ss = day['date'].split("-")
             date = datetime(int(ss[0]), int(ss[1]), int(ss[2]))
             day['date'] = date.strftime("%A,%B %d")
@@ -495,7 +600,7 @@ def daily():
         match = re.search(r'([+-]\d{2}:\d{2})$', str(datetime.now(timezone)))
         date_time[4] = match.group(1)
 
-        return render_template("daily.html", location=location, date_time=date_time, hourly=hourly)
+        return render_template("daily.html", location=location, date_time=date_time, daily=daily, unit=unit ,unit1=unit1, unit2=unit2)
 
 
 @app.route("/history")
